ef0f74d2981b5567e1b7b43bb478c81fadab56d1
Diego Lopez Araque <dlopezar@emeal.nttdata.com>
2025-03-18 16:58:19 +0100

    Se corrige lo ultimo comentado, cambiar metodos a mapper y quitar excepciones

--- SistemaPago/src/main/java/com/SistemaPago/SistemaPago/server/PaymentServiceGRPCServer.java
+++ SistemaPago/src/main/java/com/SistemaPago/SistemaPago/server/PaymentServiceGRPCServer.java
@@ -25,6 +25,7 @@ import java.util.List;
 @GrpcService
 public class PaymentServiceGRPCServer extends PaymentServiceGrpcGrpc.PaymentServiceGrpcImplBase{
 
+
     @Autowired
     private PaymentService paymentService;
 
@@ -33,67 +34,21 @@ public class PaymentServiceGRPCServer extends PaymentServiceGrpcGrpc.PaymentServ
 
     @Override
     public void registerPayment(PaymentRequest request, StreamObserver<PaymentResponse> responseObserver) {
-        try {
-            // Convertir la solicitud a un DTO de pago
-            PaymentDTO paymentDTO = requestToPaymentDTO(request);
-            System.out.println("PaymentDTO: " + paymentDTO);
+        PaymentDTO paymentDTO = paymentMapper.toDTO(request);
+        System.out.println("PaymentDTO: " + paymentDTO);
 
-            // Registrar el pago utilizando el servicio
-            PaymentDTO savedPayment = paymentService.registerPayment(paymentDTO);
+        PaymentDTO savedPayment = paymentService.registerPayment(paymentDTO);
 
-            // Enviar la respuesta al observador
-            responseObserver.onNext(paymentDtoToPaymentResponse(savedPayment));
-            responseObserver.onCompleted();
-        } catch (PaymentException e) {
-            // Manejar excepciones específicas de pago
-            System.err.println("PaymentException al registrar el pago: " + e.getMessage());
-            e.printStackTrace();
-            responseObserver.onError(new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription(e.getMessage())));
-        } catch (Exception e) {
-            // Manejar cualquier otra excepción
-            System.err.println("Error general al registrar el pago: " + e.getMessage());
-            e.printStackTrace();
-            responseObserver.onError(new StatusRuntimeException(Status.INTERNAL.withDescription("Error al registrar el pago.")));
-        }
+        responseObserver.onNext(paymentMapper.toResponse(savedPayment));
+        responseObserver.onCompleted();
     }
 
     @Override
     public void getAllPayments(Empty request, StreamObserver<PaymentResponse> responseObserver) {
-        try {
-            // Obtener todos los pagos
-            List<PaymentDTO> paymentDTOs = paymentService.getAllPayments();
-            paymentDTOs.forEach(paymentDTO -> responseObserver.onNext(paymentDtoToPaymentResponse(paymentDTO)));
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            // Manejar excepciones al recuperar pagos
-            System.err.println("Error al recuperar todos los pagos: " + e.getMessage());
-            e.printStackTrace();
-            responseObserver.onError(new StatusRuntimeException(Status.INTERNAL.withDescription("Error al recuperar todos los pagos.")));
-        }
+        List<PaymentDTO> paymentDTOs = paymentService.getAllPayments();
+        paymentDTOs.forEach(paymentDTO -> responseObserver.onNext(paymentMapper.toResponse(paymentDTO)));
+        responseObserver.onCompleted();
     }
 
-    private PaymentDTO requestToPaymentDTO(PaymentRequest request) {
-        System.out.println("PaymentRequest recibido: " + request);
-        PaymentDTO paymentDTO = new PaymentDTO();
-        paymentDTO.setCardNumber(request.getCardNumber());
-        paymentDTO.setAmount(BigDecimal.valueOf(request.getAmount()));
-        Instant instant = Instant.ofEpochSecond(request.getPaymentDate().getSeconds(), request.getPaymentDate().getNanos());
-        LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
-        paymentDTO.setPaymentDate(localDate);
-        paymentDTO.setDescription(request.getDescription());
-        System.out.println("PaymentDTO creado: " + paymentDTO);
-        return paymentDTO;
-    }
 
-    private PaymentResponse paymentDtoToPaymentResponse(PaymentDTO paymentDTO) {
-        Instant instant = paymentDTO.getPaymentDate().atStartOfDay(ZoneId.systemDefault()).toInstant();
-        Timestamp timestamp = Timestamp.newBuilder().setSeconds(instant.getEpochSecond()).setNanos(instant.getNano()).build();
-
-        return PaymentResponse.newBuilder()
-                .setCardNumber(paymentDTO.getMaskedCardNumber())
-                .setAmount(paymentDTO.getAmount().doubleValue())
-                .setPaymentDate(timestamp)
-                .setDescription(paymentDTO.getDescription())
-                .build();
-    }
 }
